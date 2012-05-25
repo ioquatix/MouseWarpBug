8fe374632db6d2a5795769f3b49a0f60d4aefb55
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-05-25 12:58:31 +1200

    Initial Commit

---
+++ MouseWarpBug/OTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OTAppDelegate.h
+//  MouseWarpBug
+//
+//  Created by Samuel Williams on 25/05/12.
+//  Copyright (c) 2012 Orion Transfer Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MouseWarpBug/OTAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  OTAppDelegate.m
+//  MouseWarpBug
+//
+//  Created by Samuel Williams on 25/05/12.
+//  Copyright (c) 2012 Orion Transfer Ltd. All rights reserved.
+//
+
+#import "OTAppDelegate.h"
+
+@implementation OTAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
